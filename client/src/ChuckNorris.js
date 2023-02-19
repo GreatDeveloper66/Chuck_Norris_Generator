fec315a19eb8df47ae42cfa73a02fdca1fdbe0a9
GreatDeveloper66 <ashaffe@gmail.com>
2023-02-19 15:51:29 -0600

    Update ChuckNorris.js

--- client/src/ChuckNorris.js
+++ client/src/ChuckNorris.js
@@ -1,33 +1,60 @@
+import React, { useState, useEffect } from 'react'
 import { Container } from 'react-bootstrap'
 import { Row } from 'react-bootstrap'
 import { Col } from 'react-bootstrap'
 import { Card } from 'react-bootstrap'
 import { Button } from 'react-bootstrap'
-import CNImage1 from "./images/chuck_norris/chuck-norris1.jpg"
-import CNImage2 from "./images/chuck_norris/chuck-norris2.jpg"
-import CNImage3 from "./images/chuck_norris/chuck-norris3.jpg"
-import CNImage4 from "./images/chuck_norris/chuck-norris4.jpg"
-import CNImage5 from "./images/chuck_norris/chuck-norris5.jpg"
-import CNImage6 from "./images/chuck_norris/chuck-norris6.jpg"
-import CNImage7 from "./images/chuck_norris/chuck-norris7.jpg"
-import CNImage8 from "./images/chuck_norris/chuck-norris8.jpg"
-import CNImage9 from "./images/chuck_norris/chuck-norris9.jpg"
-import CNImage10 from "./images/chuck_norris/chuck-norris10.jpg"
 
+function chuckNorris() {
 
+}
+
+export 
+/*
+let CNImage1 = require("./images/chuck_norris/chuck-norris1.jpg")
+let CNImage2 = require("./images/chuck_norris/chuck-norris2.jpg")
+let CNImage3 = require("./images/chuck_norris/chuck-norris3.jpg")
+let CNImage4 = require("./images/chuck_norris/chuck-norris4.jpg")
+let CNImage5 = require("./images/chuck_norris/chuck-norris4.jpg")
+let CNImage6 = require("./images/chuck_norris/chuck-norris6.jpg")
+let CNImage7 = require("./images/chuck_norris/chuck-norris7.jpg")
+let CNImage8 = require("./images/chuck_norris/chuck-norris8.jpg")
+let CNImage9 = require("./images/chuck_norris/chuck-norris9.jpg")
+let CNImage10 = require("./images/chuck_norris/chuck-norris10.jpg")
+//let imageArr = [CNImage1,CNImage2,CNImage3,CNImage4,CNImage5,CNImage6,CNImage7,CNImage8,CNImage9,CNImage10]
+*/
+/*let CNImage1 = require("./images/chuck_norris/chuck-norris1.jpg")
 function ChuckNorris() {
     const [randomInt, setRandomint] = useState(1)
     const [norrisQuote, setNorrisQuote ] = useState("Chuck Norris is the only person who can win a chess game in one move.")
-    const [imageSrc, setImageSrc ] = useState(CNImage1)
-    const imageStrings = [CNImage1,CNImage2,CNImage3,CNImage4,CNImage5,CNImage6,CNImage7,CNImage8,CNImage9,CNImage10]
-
+    const [ images, setImages ] = useState(
+        [
+            {id: 1, src: './images/chuck_norris/chuck-norris1.jpg'},
+            {id: 2, src: './images/chuck_norris/chuck-norris2.jpg'},
+            {id: 3, src: './images/chuck_norris/chuck-norris3.jpg'},
+            {id: 4, src: './images/chuck_norris/chuck-norris4.jpg'},
+            {id: 5, src: './images/chuck_norris/chuck-norris5.jpg'},
+            {id: 6, src: './images/chuck_norris/chuck-norris6.jpg'},
+            {id: 7, src: './images/chuck_norris/chuck-norris7.jpg'},
+            {id: 8, src: './images/chuck_norris/chuck-norris8.jpg'},
+            {id: 9, src: './images/chuck_norris/chuck-norris9.jpg'},
+            {id: 10, src: './images/chuck_norris/chuck-norris10.jpg'},
+        ]
+    )
+    //const [imageSrc, setImageSrc ] = useState(CNImage1)
     
     useEffect(() => {
         fetch('/chucknorrisrandomjoke')
             .then(response => response.json())
             .then(data => {
                 setNorrisQuote(data)
-                setImageSrc(imageStrings[randomInt])
+                /*
+                const image = imageArr[randomInt]
+                console.log("IMAGE IS: " + image)
+                setImageSrc(image)
+                console.log("imageSrc: " + imageSrc)
+                */
+               /*
             }).catch(err => {
                 console.log("fetch error: " + err)
             })
@@ -38,7 +65,8 @@ function ChuckNorris() {
             <Row className="justify-content-md-center">
                 <Col xs lg="2">
                     <Card>
-                    <Card.Img variant="top" src={imageSrc} alt="Card Img Top" id="chuckNorrisImage" />
+                    
+                    <img key={images[1].id} src={images[1].src}></img>
                         <Card.Body>
                             <Card.Title>Chuck Norris Generator</Card.Title>
                             <Card.Text id="chuckNorrisQuote">
@@ -53,7 +81,7 @@ function ChuckNorris() {
     )
 }
 
-export default ChuckNorris
+export { ChuckNorris }
 
 
 /*
@@ -171,7 +199,7 @@ window.addEventListener("load", event => {
 
 })
 
-
+<Card.Img variant="top" key={images[1].id}src={images[1].src} alt="Card Img Top" id="chuckNorrisImage" />
 
       */
 

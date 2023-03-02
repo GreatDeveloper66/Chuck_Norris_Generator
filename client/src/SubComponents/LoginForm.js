f4891dc36f6765aaf6eac3e5f7b9f9078ba7bed1
GreatDeveloper66 <ashaffe@gmail.com>
2023-03-01 19:47:49 -0600

    Update Login Form with mdb

--- client/src/SubComponents/LoginForm.js
+++ client/src/SubComponents/LoginForm.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { Form, Button, Alert, Container, Row, Col } from 'react-bootstrap';
+import { Form, Button, Alert, Container, Row, Col } from 'react-bootstrap'
+import { MDBBtn, MDBContainer, MDBRow, MDBCol, MDBCard, MDBCardBody, MDBInput, MDBIcon } from 'mdb-react-ui-kit'
 
 const styles = {
   formContainer: {
@@ -10,18 +11,19 @@ const styles = {
 
 
 function LoginForm() {
-  const [username, setUsername] = useState('')
+  const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [error, setError] = useState('')
 
   const handleSubmit = event => {
     event.preventDefault();
+    console.log("email: " + email + " password + " + password)
   
     // TODO: Submit form
   };
   
-  const handleUsernameChange = event => {
-    setUsername(event.target.value);
+  const handleEmailChange = event => {
+    setEmail(event.target.value);
   };
   
   const handlePasswordChange = event => {
@@ -29,32 +31,51 @@ function LoginForm() {
   };
 
   return (
-    <div style={styles.formContainer}>
-      <Container>
-        <Row>
-          <Col xs={12} md={6} lg={4}>
-            <Form onSubmit={handleSubmit}>
-            <Form.Group controlId="formBasicUsername">
-              <Form.Label>Username</Form.Label>
-              <Form.Control type="text" placeholder="Enter username" value={username} onChange={handleUsernameChange} />
-            </Form.Group>
+    <MDBContainer fluid>
 
-            <Form.Group controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Enter password" value={password} onChange={handlePasswordChange} />
-            </Form.Group>
+      <MDBRow className='d-flex justify-content-center align-items-center h-100'>
+        <MDBCol col='12'>
 
-          <Button variant="primary" type="submit">
-            Submit
-          </Button>
-            {error && <Alert variant="danger">{error}</Alert>}
-          </Form>
-         </Col>
-        </Row>
-      </Container>   
-    </div>
-    
-  );
+          <MDBCard className='bg-dark text-white my-5 mx-auto' style={{borderRadius: '1rem', maxWidth: '400px'}}>
+            <MDBCardBody className='p-5 d-flex flex-column align-items-center mx-auto w-100'>
+
+              <h2 className="fw-bold mb-2 text-uppercase">Login</h2>
+              <p className="text-white-50 mb-5">Please enter your login and password!</p>
+
+              <MDBInput wrapperClass='mb-4 mx-5 w-100' labelClass='text-white' label='Email address' id='formControlLg' type='email' size="lg" value={email} onChange={handleEmailChange}/>
+              <MDBInput wrapperClass='mb-4 mx-5 w-100' labelClass='text-white' label='Password' id='formControlLg' type='password' size="lg" value={password} onChange={handlePasswordChange}/>
+
+              <p className="small mb-3 pb-lg-2"><a class="text-white-50" href="#!">Forgot password?</a></p>
+              <MDBBtn outline className='mx-2 px-5' color='white' size='lg' onClick = {handleSubmit}>
+                Login
+              </MDBBtn>
+
+              <div className='d-flex flex-row mt-3 mb-5'>
+                <MDBBtn tag='a' color='none' className='m-3' style={{ color: 'white' }}>
+                  <MDBIcon fab icon='facebook-f' size="lg"/>
+                </MDBBtn>
+
+                <MDBBtn tag='a' color='none' className='m-3' style={{ color: 'white' }}>
+                  <MDBIcon fab icon='twitter' size="lg"/>
+                </MDBBtn>
+
+                <MDBBtn tag='a' color='none' className='m-3' style={{ color: 'white' }}>
+                  <MDBIcon fab icon='google' size="lg"/>
+                </MDBBtn>
+              </div>
+
+              <div>
+                <p className="mb-0">Don't have an account? <a href="#!" className="text-white-50 fw-bold">Sign Up</a></p>
+
+              </div>
+            </MDBCardBody>
+          </MDBCard>
+
+        </MDBCol>
+      </MDBRow>
+
+    </MDBContainer>
+  )
 }
 
 export default LoginForm

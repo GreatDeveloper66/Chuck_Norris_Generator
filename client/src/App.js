21e8dd981bce0913710e95b27df5a7d7700e05e8
GreatDeveloper66 <ashaffe@gmail.com>
2023-03-04 22:33:52 -0600

    LoginForm Rendering Through App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,14 @@
 
 import React from 'react';
-import { Route, Router, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Routes , Route, Link } from 'react-router-dom';
+
 //import RegisterForm from './SubComponents/RegisterForm';
 import LoginForm from './SubComponents/LoginForm';
 import ChuckNorris from './SubComponents/ChuckNorris'
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <LoginForm />
-        </Route>
-        <Route exact path="/chuck_norris">
-          <ChuckNorris />
-        </Route>
-      </Switch>
-    </Router>
+    <LoginForm/>
   );
 }
 
@@ -24,31 +16,3 @@ function App() {
 
 export default App
 
-/*
-import React from 'react';
-import LoginForm from './SubComponents/LoginForm.js'
-
-function App() {
-  return (
-
-   <LoginForm />
-  );
-}
-
-export default App;
-
-
-
-    <BrowserRouter>
-      <div>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/about" component={AboutPage} />
-      </div>
-    </BrowserRouter>
-
-
-    import { BrowserRouter, Route } from 'react-router-dom';
-import HomePage from './HomePage';
-import AboutPage from './AboutPage';
-
-*/
